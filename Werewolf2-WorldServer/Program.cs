8c31d1d0681fe59ac081bf4d4a7630598848509f
Michitaro Naito <m-naito@amlitek.com>
2014-01-27 01:29:43 +0900

    Initial Solution

---
+++ Werewolf2-WorldServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werewolf2_WorldServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Werewolf2-WorldServer Initial Commit");
+            Console.Read();
+        }
+    }
+}
